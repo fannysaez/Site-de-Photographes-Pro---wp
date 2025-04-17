9b3dcaec307741b628591a7b7fbb64a86ba15ca5
saez.fanny.63@gmail.com <saez.fanny.63@gmail.com>
2025-04-17 14:54:39 +0200

    add + mise à jour

--- wp-content/plugins/🐦‍⬛/🐦‍⬛.js
+++ wp-content/plugins/🐦‍⬛/🐦‍⬛.js
@@ -1,19 +1,20 @@
 const button = document.querySelector('.🐦‍⬛🐦‍⬛')
 
-console.log(button)
-button.addEventListener('change', () => {
+button.addEventListener('click', () => {
     if (button.getAttribute('data-checked') == 'true') {
-        button.setAttribute('data-checked') = 'false'
+        button.setAttribute('data-checked', 'false')
     } else {
-        button.setAttribute('data-checked') = 'true'
+        button.setAttribute('data-checked', 'true')
+        alert("Vous venez d'activer le mode destruction")
     }
 })
 
-if (button.getAttribute('data-checked') == 'true') {
-    alert("Vous venez d'activer le mode destruction")
-    document.body.addEventListener('click', (e) => {
-        if (e.target != button && !e.target.matches('.🐦‍⬛🐦‍⬛')) {
-            e.target.remove()
-        }
-    })
-}
+document.addEventListener('click', () => {
+    if (button.getAttribute('data-checked') == 'true') {
+        document.body.addEventListener('click', (e) => {
+            if (e.target != button && !e.target.matches('.🐦‍⬛🐦‍⬛')) {
+                e.target.remove()
+            }
+        })
+    }
+})
